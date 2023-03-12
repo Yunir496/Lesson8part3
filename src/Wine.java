da12692c5df3267de89b85d355942ad74f89504c
Yunir <spire47911@yandex.ru>
2023-03-12 10:11:15 +0500

    comit 8.3  v2

--- src/Wine.java
+++ src/Wine.java
@@ -7,95 +7,67 @@
         3. Расчёт выдержки вина (текущая дата даётся как аргумент).
         Отдельно разработайте вспомогательный класс, который демонстрирует работоспособность вашего класса.*/
 
-import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.concurrent.TimeUnit;
+import java.time.LocalDate;
+import java.time.Period;
 
 public class Wine {
        private String name;
        private String trademark;
        private String country;
-       private String dataOfBotting;
+       private LocalDate dataOfBotting;
        private  String note;
 
-  /*     public long calcexcerpt (String data) throws ParseException {
-              SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
-              Date date1 = null;
-              Date date2 = null;
-               date1 = format.parse(data);
-               date2 = format.parse(dataOfBotting);
+        public int calcExcerpt(){
+            LocalDate date2 = LocalDate.now();
+            Period period = Period.between(dataOfBotting,date2);
+            return  period.getYears();
+        }
 
-               long difference = date2.getTime()-date1.getTime();
+    public Wine(String name, String trademark, String country, LocalDate dataOfBotting, String note) {
+        this.name = name;
+        this.trademark = trademark;
+        this.country = country;
+        this.dataOfBotting = dataOfBotting;
+        this.note = note;
+    }
 
-              return difference / (24 * 60 * 60 * 1000);
-       }*/
-  public long calcexcerpt (String curDate) {
+    public String getName() {
+        return name;
+    }
 
-         SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
-         Date date1 = null;
-         Date date2 = null;
-         try {
-                date1 = format.parse(curDate);
-                date2 = format.parse(dataOfBotting);
-         } catch (java.text.ParseException e) {
-                e.printStackTrace();
-         }
-      assert date2 != null;
-        
-      long difference = Math.abs(date1.getTime() - date2.getTime());
+    public void setName(String name) {
+        this.name = name;
+    }
 
-      long diff = TimeUnit.DAYS.convert(difference, TimeUnit.MILLISECONDS);
-      //long days =  difference / (24 * 60 * 60 * 1000);
-         return diff;
-  }
-       public Wine(String name, String trademark, String country, String dataOfBotting, String note) {
-              this.name = name;
-              this.trademark = trademark;
-              this.country = country;
-              this.dataOfBotting = dataOfBotting;
-              this.note = note;
-       }
+    public String getTrademark() {
+        return trademark;
+    }
 
-       public String getName() {
-              return name;
-       }
+    public void setTrademark(String trademark) {
+        this.trademark = trademark;
+    }
 
-       public void setName(String name) {
-              this.name = name;
-       }
+    public String getCountry() {
+        return country;
+    }
 
-       public String getTrademark() {
-              return trademark;
-       }
+    public void setCountry(String country) {
+        this.country = country;
+    }
 
-       public void setTrademark(String trademark) {
-              this.trademark = trademark;
-       }
+    public LocalDate getDataOfBotting() {
+        return dataOfBotting;
+    }
 
-       public String getCountry() {
-              return country;
-       }
+    public void setDataOfBotting(LocalDate dataOfBotting) {
+        this.dataOfBotting = dataOfBotting;
+    }
 
-       public void setCountry(String country) {
-              this.country = country;
-       }
+    public String getNote() {
+        return note;
+    }
 
-       public String getDataOfBotting() {
-              return dataOfBotting;
-       }
-
-       public void setDataOfBotting(String dataOfBotting) {
-              this.dataOfBotting = dataOfBotting;
-       }
-
-       public String getNote() {
-              return note;
-       }
-
-       public void setNote(String note) {
-              this.note = note;
-       }
+    public void setNote(String note) {
+        this.note = note;
+    }
 }
